d886ef3281e96e850cdd8f03b4b98b16bac7df3c
Mart drives_a_ford Ratas <mart@ratas.dev>
2021-06-08 20:54:15 +0300

    Intercept event now keeps track of its non-internal stack traces along with the corresponding plugins (where available)

--- src/main/java/me/lucko/networkinterceptor/InterceptEvent.java
+++ src/main/java/me/lucko/networkinterceptor/InterceptEvent.java
@@ -1,12 +1,20 @@
 package me.lucko.networkinterceptor;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class InterceptEvent {
     private final String host;
     private final StackTraceElement[] stackTrace;
+    private final Map<StackTraceElement, JavaPlugin> nonInternalStackTrace = new HashMap<>();
 
     public InterceptEvent(String host, StackTraceElement[] stackTrace) {
         this.host = host;
         this.stackTrace = stackTrace;
+        generateNonInternalStackTrace();
     }
 
     public String getHost() {
@@ -15,5 +23,41 @@ public class InterceptEvent {
 
     public StackTraceElement[] getStackTrace() {
         return this.stackTrace;
+    }
+
+    public Map<StackTraceElement, JavaPlugin> getNonInternalStackTraceWithPlugins() {
+        return Collections.unmodifiableMap(nonInternalStackTrace);
+    }
+
+    private void generateNonInternalStackTrace() {
+        boolean shouldPrint = false;
+        for (StackTraceElement element : stackTrace) {
+            if (!shouldPrint) {
+                boolean isInternal = element.getClassName().startsWith("me.lucko.networkinterceptor")
+                        || element.getClassName().startsWith("java.net")
+                        || element.getClassName().startsWith("java.security")
+                        || element.getClassName().startsWith("sun.net")
+                        || element.getClassName().startsWith("sun.security.ssl");
+
+                if (!isInternal) {
+                    shouldPrint = true;
+                }
+            }
+
+            if (shouldPrint) {
+                // sb.append("\tat ").append(element);
+                JavaPlugin providingPlugin;
+                try {
+                    // append the name of the plugin
+                    Class<?> clazz = Class.forName(element.getClassName());
+                    providingPlugin = JavaPlugin.getProvidingPlugin(clazz);
+                    // sb.append(" [").append(providingPlugin.getName()).append(']');
+                } catch (Exception e) {
+                    // ignore
+                    providingPlugin = null;
+                }
+                nonInternalStackTrace.put(element, providingPlugin);
+            }
+        }
     }
 }
--- src/main/java/me/lucko/networkinterceptor/loggers/AbstractEventLogger.java
+++ src/main/java/me/lucko/networkinterceptor/loggers/AbstractEventLogger.java
@@ -4,6 +4,7 @@ import me.lucko.networkinterceptor.InterceptEvent;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 public abstract class AbstractEventLogger implements EventLogger {
@@ -18,38 +19,17 @@ public abstract class AbstractEventLogger implements EventLogger {
     @Override
     public void logAttempt(InterceptEvent event) {
         String host = event.getHost();
-        StackTraceElement[] stackTrace = event.getStackTrace();
 
         StringBuilder sb = new StringBuilder("Intercepted outgoing connection to host '").append(host).append("'\n");
 
         // print stacktrace
         if (this.includeTraces) {
-            boolean shouldPrint = false;
-            for (StackTraceElement element : stackTrace) {
-                if (!shouldPrint) {
-                    boolean isInternal = element.getClassName().startsWith("me.lucko.networkinterceptor") ||
-                            element.getClassName().startsWith("java.net") ||
-                            element.getClassName().startsWith("java.security") ||
-                            element.getClassName().startsWith("sun.net") ||
-                            element.getClassName().startsWith("sun.security.ssl");
-
-                    if (!isInternal) {
-                        shouldPrint = true;
-                    }
-                }
-
-                if (shouldPrint) {
-                    sb.append("\tat ").append(element);
-                    try {
-                        // append the name of the plugin
-                        Class<?> clazz = Class.forName(element.getClassName());
-                        JavaPlugin providingPlugin = JavaPlugin.getProvidingPlugin(clazz);
-                        sb.append(" [").append(providingPlugin.getName()).append(']');
-                    } catch (Exception e) {
-                        // ignore
-                    }
-                    sb.append("\n");
-                }
+            Map<StackTraceElement, JavaPlugin> map = event.getNonInternalStackTraceWithPlugins();
+            for (StackTraceElement element : map.keySet()) {
+                sb.append("\tat ").append(element);
+                JavaPlugin providingPlugin = map.get(element);
+                sb.append(" [").append(providingPlugin == null ? "N/A" : providingPlugin.getName()).append(']');
+                sb.append("\n");
             }
         }
 
