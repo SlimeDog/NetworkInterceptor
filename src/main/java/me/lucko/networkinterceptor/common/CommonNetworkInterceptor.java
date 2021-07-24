3a7547c664454b40613ec11a02d3d7e5639b5382
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-07-24 18:13:01 +0300

    Added velocity plugin options class

--- src/main/java/me/lucko/networkinterceptor/common/CommonNetworkInterceptor.java
+++ src/main/java/me/lucko/networkinterceptor/common/CommonNetworkInterceptor.java
@@ -36,6 +36,8 @@ import me.lucko.networkinterceptor.loggers.FileLogger;
 import me.lucko.networkinterceptor.plugin.KeepPlugins;
 import me.lucko.networkinterceptor.plugin.ManualPluginOptions;
 import me.lucko.networkinterceptor.plugin.PluginOptions;
+import me.lucko.networkinterceptor.velocity.VelocityNetworkInterceptor;
+import me.lucko.networkinterceptor.velocity.VelocityPluginOptions;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class CommonNetworkInterceptor<T extends NetworkInterceptorPlugin<PLUGIN>, PLUGIN> {
@@ -292,6 +294,11 @@ public class CommonNetworkInterceptor<T extends NetworkInterceptorPlugin<PLUGIN>
             @SuppressWarnings("unchecked")
             PluginOptions<PLUGIN> opts = (PluginOptions<PLUGIN>) new BungeePluginOptions<Plugin>((Plugin) plugin,
                     keepType, allowNonPlugin, trustedPlugins);
+            return opts;
+        } else if (plugin.isVelocity()) {
+            @SuppressWarnings("unchecked")
+            PluginOptions<PLUGIN> opts = (PluginOptions<PLUGIN>) new VelocityPluginOptions(
+                    (VelocityNetworkInterceptor) plugin, keepType, allowNonPlugin, trustedPlugins);
             return opts;
         }
         throw new IllegalStateException("Unknown type of plugin: " + plugin);
