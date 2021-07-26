96c8167b2ad000b4ac8c67da52596d229d39aab7
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-07-26 23:57:24 +0300

    Traversing parent loggers for file logger and setting all their levels to ALL

--- src/main/java/me/lucko/networkinterceptor/loggers/FileLogger.java
+++ src/main/java/me/lucko/networkinterceptor/loggers/FileLogger.java
@@ -47,12 +47,13 @@ public class FileLogger<PLUGIN> extends AbstractEventLogger<PLUGIN> {
         System.out.println("File logger uses logger: " + this.logger);
         System.out.println("Logger level: " + this.logger.getLevel());
         Logger parent = this.logger.getParent();
-        if (parent != null) {
+        while (parent != null) {
             parent.setLevel(Level.ALL);
             parent.setFilter(record -> true);
+            System.out.println("Parent logger for file logger: " + parent);
+            System.out.println("Parent logger level: " + parent.getLevel());
+            parent = parent.getParent();
         }
-        System.out.println("Parent logger for file logger: " + parent);
-        System.out.println("Parent logger level: " + parent.getLevel());
     }
 
     @Override
