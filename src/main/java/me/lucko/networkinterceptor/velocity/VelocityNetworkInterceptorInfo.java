9a9693b8137a4c2fdf7accc2c467e2a850f7deda
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-07-27 16:30:33 +0300

    Version parity for velocity

--- src/main/java/me/lucko/networkinterceptor/velocity/VelocityNetworkInterceptorInfo.java
+++ src/main/java/me/lucko/networkinterceptor/velocity/VelocityNetworkInterceptorInfo.java
@@ -1,7 +1,7 @@
 package me.lucko.networkinterceptor.velocity;
 
 public final class VelocityNetworkInterceptorInfo {
-    static final String VERSION = "3.0.0";
+    static final String VERSION = "3.0.0-Vel-3";
     static final String NAME = "NetworkInterceptor";
     static final String ID = "networkinterceptor";
     static final String DESCRIPTION = "Plugin to monitor and block outgoing network requests";
