5c2733416ba39ba926acb11f5b645bc7839b2cc7
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2021-07-29 13:59:00 +0300

    Version bump. v3.1.2

--- src/main/java/me/lucko/networkinterceptor/velocity/VelocityNetworkInterceptorInfo.java
+++ src/main/java/me/lucko/networkinterceptor/velocity/VelocityNetworkInterceptorInfo.java
@@ -1,7 +1,7 @@
 package me.lucko.networkinterceptor.velocity;
 
 public final class VelocityNetworkInterceptorInfo {
-    static final String VERSION = "3.1.1";
+    static final String VERSION = "3.1.2";
     static final String NAME = "NetworkInterceptor";
     static final String ID = "networkinterceptor";
     static final String DESCRIPTION = "Plugin to monitor and block outgoing network requests";
