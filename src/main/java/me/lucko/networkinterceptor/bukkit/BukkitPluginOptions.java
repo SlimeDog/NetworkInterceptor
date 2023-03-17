1b80db6beed12dc39e84f40decae44ec52d3806f
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2023-03-17 09:11:36 +0000

    Fix overly eager SlimeDogPlugin integration for plugin options

--- src/main/java/me/lucko/networkinterceptor/bukkit/BukkitPluginOptions.java
+++ src/main/java/me/lucko/networkinterceptor/bukkit/BukkitPluginOptions.java
@@ -3,6 +3,7 @@ package me.lucko.networkinterceptor.bukkit;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
 
 import dev.ratas.slimedogcore.api.SlimeDogPlugin;
@@ -25,7 +26,7 @@ public class BukkitPluginOptions<T extends SlimeDogPlugin> extends PluginOptions
         if (plugin == null) {
             return false;
         }
-        if (!(plugin instanceof SlimeDogPlugin)) {
+        if (!(plugin instanceof JavaPlugin)) {
             owner.getLogger().warning("Plugin of unknown type (" + name + "): " + plugin);
             return false;
         }
